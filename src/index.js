aba3b62d21a6d3777d38b085e028092cc4a16b9a
Q4D4 <dato.kadaria.1@btu.edu.ge>
2022-12-16 00:23:32 +0400

    Changed global css loading order

--- src/index.js
+++ src/index.js
@@ -3,8 +3,8 @@ import ReactDOM from 'react-dom/client';
 import { BrowserRouter } from 'react-router-dom';
 import App from './App';
 // Global stylesheets
-import './styles/main.css';
 import './styles/theme.css';
+import './styles/main.css';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
