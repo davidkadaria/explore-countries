89a16c6461d2b3ef5f233701b228899ab853d33f
Q4D4 <dato.kadaria.1@btu.edu.ge>
2022-12-16 01:31:46 +0400

    Structured App component

--- src/App.js
+++ src/App.js
@@ -1,10 +1,44 @@
-import { Header } from './components';
+import axios from 'axios';
+import { useState, useEffect, Fragment, Suspense, lazy } from 'react';
+import { Routes, Route } from 'react-router-dom';
+
+import { Header, MainWrapper } from './components';
+
+const Home = lazy(() => import('./pages/Home'));
+const Detail = lazy(() => import('./pages/Detail'));
 
 function App() {
+	const [countries, setCountries] = useState();
+
+	useEffect(() => {
+		axios.get('https://restcountries.com/v3.1/all').then((res) => {
+			setCountries(res.data);
+		});
+	}, []);
+
+	const getCountryById = (id) => {
+		return countries.find((country) => country.id === id);
+	};
+
 	return (
-		<div>
+		<Fragment>
 			<Header />
-		</div>
+			<MainWrapper>
+				{countries ? (
+					<Suspense fallback={<div>Loading...</div>}>
+						<Routes>
+							<Route path="/" element={<Home countries={countries} />} />
+							<Route
+								path="/detail/:id"
+								element={<Detail getCountryById={getCountryById} />}
+							/>
+						</Routes>
+					</Suspense>
+				) : (
+					<div>Still loading...</div>
+				)}
+			</MainWrapper>
+		</Fragment>
 	);
 }
 
