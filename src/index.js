6ca5cbe900b40f046459f0916ec0e975c4ae3973
Q4D4 <dato.kadaria.1@btu.edu.ge>
2022-12-15 21:42:11 +0400

    Added and imported global styles

--- src/index.js
+++ src/index.js
@@ -2,6 +2,9 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import { BrowserRouter } from 'react-router-dom';
 import App from './App';
+// Global stylesheets
+import './styles/main.css';
+import './styles/theme.css';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
