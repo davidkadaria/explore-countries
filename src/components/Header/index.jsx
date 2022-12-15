2b14c0fbb915739ba89ed3c236fef4f0f161a3f0
Q4D4 <dato.kadaria.1@btu.edu.ge>
2022-12-16 00:23:12 +0400

    Modified Header component and its styles

--- src/components/Header/index.jsx
+++ src/components/Header/index.jsx
@@ -1,7 +1,14 @@
 import './Header.css';
 
 function Header() {
-	return <header className="Header">Header</header>;
+	return (
+		<header className="Header">
+			<div className="Header__container">
+				<h1 className="Header__heading">Where in the world?</h1>
+				<div className="Header__theme-switch">Dark Mode</div>
+			</div>
+		</header>
+	);
 }
 
 export { Header };
