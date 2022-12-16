18a42aa4d83b0d154004480421bcb2ca41facf9e
Q4D4 <dato.kadaria.1@btu.edu.ge>
2022-12-16 10:42:48 +0400

    Modified CountryCard component

--- src/components/CountryCard/index.jsx
+++ src/components/CountryCard/index.jsx
@@ -1,7 +1,25 @@
 import './CountryCard.css';
 
-function CountryCard() {
-	return <div>Card</div>;
+function CountryCard({ flag, name, population, region, capital }) {
+	return (
+		<div className="CountryCard">
+			<div className="CountryCard__flag">
+				<img src={flag} alt={`Flag of ${name}`} />
+			</div>
+			<div className="CountryCard__info">
+				<h2 className="CountryCard__name">{name}</h2>
+				<p className="CountryCard__prop">
+					Population: <span>{population}</span>
+				</p>
+				<p className="CountryCard__prop">
+					Region: <span>{region}</span>
+				</p>
+				<p className="CountryCard__prop">
+					Capital: <span>{capital}</span>
+				</p>
+			</div>
+		</div>
+	);
 }
 
 export { CountryCard };
