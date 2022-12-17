319cd4e979a20bf9aef5c3e9f420c32c7e6c3342
Q4D4 <dato.kadaria.1@btu.edu.ge>
2022-12-17 16:29:33 +0400

    Some changes in HomePage

--- src/pages/Home/index.jsx
+++ src/pages/Home/index.jsx
@@ -1,4 +1,4 @@
-import { Fragment } from 'react';
+import { useState, Fragment } from 'react';
 import {
 	CountryCardsWrapper,
 	CountryCard,
@@ -8,23 +8,55 @@ import {
 } from '../../components';
 
 function Home({ countries }) {
+	const [searchValue, setSearchValue] = useState('');
+	const [region, setRegion] = useState('');
+
+	const filter = (countries) => {
+		if (region !== '' && searchValue !== '') {
+			return countries.filter((country) => {
+				return (
+					country.region === region &&
+					country.name.common.toLowerCase().includes(searchValue)
+				);
+			});
+		} else if (region !== '' && searchValue === '') {
+			return countries.filter((country) => {
+				return country.region === region;
+			});
+		} else if (region === '' && searchValue !== '') {
+			return countries.filter((country) => {
+				return country.name.common.toLowerCase().includes(searchValue);
+			});
+		} else {
+			return countries;
+		}
+	};
+
 	return (
 		<Fragment>
 			<FilterWrapper>
-				<Search />
-				<Dropdown />
+				<Search setValue={setSearchValue} />
+				<Dropdown currentRegion={region} setRegion={setRegion} />
 			</FilterWrapper>
 			<CountryCardsWrapper>
-				{countries.map((country) => (
-					<CountryCard
-						key={`${country.region}/${country.name.common}`}
-						flag={country.flags.svg}
-						name={country.name.common}
-						population={country.population}
-						region={country.region}
-						capital={country.capital?.join()}
-					/>
-				))}
+				{countries ? (
+					countries.length === 0 ? (
+						<div>404</div>
+					) : (
+						filter(countries).map((country) => (
+							<CountryCard
+								key={`${country.region}/${country.name.common}`}
+								flag={country.flags.svg}
+								name={country.name.common}
+								population={country.population}
+								region={country.region}
+								capital={country.capital?.join()}
+							/>
+						))
+					)
+				) : (
+					<div>loading...</div>
+				)}
 			</CountryCardsWrapper>
 		</Fragment>
 	);
