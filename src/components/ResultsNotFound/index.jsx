89b9abd6642374e940cecfa9af4b763bdaac7253
Q4D4 <dato.kadaria.1@btu.edu.ge>
2022-12-17 23:41:24 +0400

    Modified ResultsNotFound component and its styles

--- src/components/ResultsNotFound/index.jsx
+++ src/components/ResultsNotFound/index.jsx
@@ -1,7 +1,7 @@
 import './ResultsNotFound.css';
 
 function ResultsNotFound() {
-	return <h2 className="ResultsNotFound">No countries found</h2>;
+	return <h2 className="ResultsNotFound">No countries found :(</h2>;
 }
 
 export { ResultsNotFound };
