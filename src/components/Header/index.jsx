a611bd99f3075fc8acea2d49d9b71b0acb17c80b
Q4D4 <dato.kadaria.1@btu.edu.ge>
2022-12-17 21:50:17 +0400

    Modified Header: added link to home page

--- src/components/Header/index.jsx
+++ src/components/Header/index.jsx
@@ -1,10 +1,13 @@
+import { Link } from 'react-router-dom';
 import './Header.css';
 
 function Header() {
 	return (
 		<header className="Header">
 			<div className="Header__container">
-				<h1 className="Header__heading">Where in the world?</h1>
+				<Link to="/">
+					<h1 className="Header__heading">Where in the world?</h1>
+				</Link>
 				<div className="Header__theme-switch">Dark Mode</div>
 			</div>
 		</header>
