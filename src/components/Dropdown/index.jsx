adf5e7a23d510f0a067898b6769c36bc575c85ce
Q4D4 <dato.kadaria.1@btu.edu.ge>
2022-12-17 16:27:55 +0400

    Modified Dropdown component and its stylesheets

--- src/components/Dropdown/index.jsx
+++ src/components/Dropdown/index.jsx
@@ -1,56 +1,41 @@
-import { useState, useEffect } from 'react';
+import { useState } from 'react';
 import { ChevronDownIcon } from '../../icons';
 import './Dropdown.css';
 
-const regionsList = [
-	{ id: 1, name: 'Africa', selected: false },
-	{ id: 2, name: 'America', selected: false },
-	{ id: 3, name: 'Asia', selected: false },
-	{ id: 4, name: 'Europe', selected: false },
-	{ id: 5, name: 'Oceania', selected: false },
-];
+const regionsList = ['Africa', 'Americas', 'Asia', 'Europe', 'Oceania'];
 
-function Dropdown() {
+function Dropdown({ currentRegion, setRegion }) {
 	const [isOpen, setIsOpen] = useState(false);
-	const [regions, setRegions] = useState(regionsList);
-	let mountedOnce = false;
-
-	useEffect(() => {
-		!mountedOnce ? (mountedOnce = true) : console.log('second');
-	}, [regions]);
 
 	const toggleMenu = () => {
 		setIsOpen((prevState) => !prevState);
 	};
 
-	const toggleSelect = (id) => {
-		setRegions((prevState) => {
-			const newState = prevState.map((region) => {
-				if (region.id === id) {
-					return { ...region, selected: !region.selected };
-				}
-				return { ...region };
-			});
-			return newState;
-		});
+	const toggleSelect = (region) => {
+		if (region === currentRegion) {
+			setRegion('');
+		} else {
+			setRegion(region);
+		}
+		setIsOpen(false);
 	};
 
 	return (
 		<div className="Dropdown">
 			<button className="Dropdown__button" onClick={toggleMenu}>
-				Filter by Region
+				{currentRegion || 'Filter by Region'}
 			</button>
 			<ChevronDownIcon />
 			<ul className={`Dropdown__menu ${isOpen ? 'Dropdown__menu--open' : ''}`}>
-				{regions.map((region) => (
+				{regionsList.map((region) => (
 					<li
 						className={`Dropdown__item ${
-							region.selected ? 'Dropdown__item--selected' : ''
+							region === currentRegion ? 'Dropdown__item--selected' : ''
 						}`}
-						key={region.id}
-						onClick={() => toggleSelect(region.id)}
+						key={region}
+						onClick={() => toggleSelect(region)}
 					>
-						{region.name}
+						{region}
 					</li>
 				))}
 			</ul>
