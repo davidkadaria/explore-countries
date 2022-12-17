5195cbc03771cbcb3a95602479e767aa29314034
Q4D4 <dato.kadaria.1@btu.edu.ge>
2022-12-17 22:23:59 +0400

    ADded responsive media queries for detail page

--- src/pages/Detail/index.jsx
+++ src/pages/Detail/index.jsx
@@ -25,55 +25,60 @@ function Detail({ getCountryByRegionAndName, getBorderCountriesByCountryCodes })
 					src={countryData.flags.svg}
 					alt={`National flag of ${countryData.name.common}`}
 				/>
-				<h1 className="Detail__name">{countryData.name.common}</h1>
-				<div className="Detail__info">
-					<div className="Detail__info-group">
-						<CountryInfo
-							prefix="Native name"
-							value={Object.values(countryData.name.nativeName)
-								.map((obj) => obj.common)
-								.join(', ')}
-						/>
-						<CountryInfo
-							prefix="Population"
-							value={countryData.population.toLocaleString('en-US')}
-						/>
-						<CountryInfo prefix="Region" value={countryData.region} />
-						<CountryInfo prefix="Sub region" value={countryData.subregion} />
-						<CountryInfo prefix="Capital" value={countryData.capital?.join(', ')} />
-					</div>
-					<div className="Detail__info-group">
-						<CountryInfo prefix="Top level domain" value={countryData.tld?.join(', ')} />
-						<CountryInfo
-							prefix="Currencies"
-							value={Object.values(countryData.currencies)
-								.map((obj) => obj.name)
-								?.join(', ')}
-						/>
-						<CountryInfo
-							prefix="Languages"
-							value={Object.values(countryData.languages)?.join(', ')}
-						/>
-					</div>
-				</div>
-				{borderCountries.length > 0 ? (
-					<div className="Detail__borders">
-						<h3 className="Detail__borders-prefix">Border Countries: </h3>
-						<div className="Detail__borders-list">
-							{borderCountries.map((country) => (
-								<Button
-									key={`${country.region}/${country.name.common}`}
-									textContent={country.name.common}
-									clickHandler={() =>
-										navigate(
-											`/${country.region.toLowerCase()}/${country.name.common.toLowerCase()}`
-										)
-									}
-								/>
-							))}
+				<div>
+					<h1 className="Detail__name">{countryData.name.common}</h1>
+					<div className="Detail__info">
+						<div className="Detail__info-group">
+							<CountryInfo
+								prefix="Native name"
+								value={Object.values(countryData.name.nativeName)
+									.map((obj) => obj.common)
+									.join(', ')}
+							/>
+							<CountryInfo
+								prefix="Population"
+								value={countryData.population.toLocaleString('en-US')}
+							/>
+							<CountryInfo prefix="Region" value={countryData.region} />
+							<CountryInfo prefix="Sub region" value={countryData.subregion} />
+							<CountryInfo prefix="Capital" value={countryData.capital?.join(', ')} />
+						</div>
+						<div className="Detail__info-group">
+							<CountryInfo
+								prefix="Top level domain"
+								value={countryData.tld?.join(', ')}
+							/>
+							<CountryInfo
+								prefix="Currencies"
+								value={Object.values(countryData.currencies)
+									.map((obj) => obj.name)
+									?.join(', ')}
+							/>
+							<CountryInfo
+								prefix="Languages"
+								value={Object.values(countryData.languages)?.join(', ')}
+							/>
 						</div>
 					</div>
-				) : null}
+					{borderCountries.length > 0 ? (
+						<div className="Detail__borders">
+							<h3 className="Detail__borders-prefix">Border Countries: </h3>
+							<div className="Detail__borders-list">
+								{borderCountries.map((country) => (
+									<Button
+										key={`${country.region}/${country.name.common}`}
+										textContent={country.name.common}
+										clickHandler={() =>
+											navigate(
+												`/${country.region.toLowerCase()}/${country.name.common.toLowerCase()}`
+											)
+										}
+									/>
+								))}
+							</div>
+						</div>
+					) : null}
+				</div>
 			</div>
 		</Fragment>
 	);
