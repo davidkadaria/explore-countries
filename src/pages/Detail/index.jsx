40aa2cb40c6fa4445aecacda5183e416ca2ef18e
Q4D4 <dato.kadaria.1@btu.edu.ge>
2022-12-17 21:43:03 +0400

    Modified Detail page and its styles

--- src/pages/Detail/index.jsx
+++ src/pages/Detail/index.jsx
@@ -4,24 +4,30 @@ import { Button, CountryInfo } from '../../components';
 import { ArrowLeftIcon } from '../../icons';
 import './Detail.css';
 
-function Detail({ getCountryByRegionAndName }) {
+function Detail({ getCountryByRegionAndName, getBorderCountriesByCountryCodes }) {
 	const navigate = useNavigate();
 	const { region, name } = useParams();
 
 	const countryData = getCountryByRegionAndName(region, name);
-	console.log(countryData);
+	const borderCountries = getBorderCountriesByCountryCodes(countryData.borders);
+	// console.log(countryData);
+	// console.log(borderCountries);
 	return (
 		<Fragment>
 			<Button
 				textContent="Back"
 				icon={<ArrowLeftIcon />}
-				clickHandler={() => navigate('/')}
+				clickHandler={() => navigate(-1)}
 			/>
 			<div className="Detail">
-				<img className="Detail__flag" src={countryData.flags.svg} alt="" />
+				<img
+					className="Detail__flag"
+					src={countryData.flags.svg}
+					alt={`National flag of ${countryData.name.common}`}
+				/>
 				<h1 className="Detail__name">{countryData.name.common}</h1>
 				<div className="Detail__info">
-					<div className="Detail__info-basic">
+					<div className="Detail__info-group">
 						<CountryInfo
 							prefix="Native name"
 							value={Object.values(countryData.name.nativeName)
@@ -36,11 +42,13 @@ function Detail({ getCountryByRegionAndName }) {
 						<CountryInfo prefix="Sub region" value={countryData.subregion} />
 						<CountryInfo prefix="Capital" value={countryData.capital?.join(', ')} />
 					</div>
-					<div className="Detail__info-additional">
+					<div className="Detail__info-group">
 						<CountryInfo prefix="Top level domain" value={countryData.tld?.join(', ')} />
 						<CountryInfo
 							prefix="Currencies"
-							value={Object.keys(countryData.currencies)?.join(', ')}
+							value={Object.values(countryData.currencies)
+								.map((obj) => obj.name)
+								?.join(', ')}
 						/>
 						<CountryInfo
 							prefix="Languages"
@@ -48,6 +56,24 @@ function Detail({ getCountryByRegionAndName }) {
 						/>
 					</div>
 				</div>
+				{borderCountries.length > 0 ? (
+					<div className="Detail__borders">
+						<h3 className="Detail__borders-prefix">Border Countries: </h3>
+						<div className="Detail__borders-list">
+							{borderCountries.map((country) => (
+								<Button
+									key={`${country.region}/${country.name.common}`}
+									textContent={country.name.common}
+									clickHandler={() =>
+										navigate(
+											`/${country.region.toLowerCase()}/${country.name.common.toLowerCase()}`
+										)
+									}
+								/>
+							))}
+						</div>
+					</div>
+				) : null}
 			</div>
 		</Fragment>
 	);
