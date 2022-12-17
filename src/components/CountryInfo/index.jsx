6d42c05409c2378b4b211c7bf6f6db18155ed4ea
Q4D4 <dato.kadaria.1@btu.edu.ge>
2022-12-17 20:43:02 +0400

    Modified CountryInfo component and its styles

--- src/components/CountryInfo/index.jsx
+++ src/components/CountryInfo/index.jsx
@@ -1,6 +1,9 @@
 import './CountryInfo.css';
 
 function CountryInfo({ prefix, value }) {
+	if (!value) {
+		return null;
+	}
 	return (
 		<p className="CountryInfo">
 			{prefix}: <span className="CountryInfo__value">{value}</span>
