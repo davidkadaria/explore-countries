3f13226b8a3af6ef0df6f2f80f792e506a92f128
Q4D4 <dato.kadaria.1@btu.edu.ge>
2022-12-17 18:42:05 +0400

    Modified Detail page

--- src/pages/Detail/index.jsx
+++ src/pages/Detail/index.jsx
@@ -1,10 +1,23 @@
-import { useParams } from 'react-router-dom';
+import { Fragment } from 'react';
+import { useNavigate, useParams } from 'react-router-dom';
+import { Button } from '../../components';
+import { ArrowLeftIcon } from '../../icons';
 
 function Detail({ getCountryByRegionAndName }) {
+	const navigate = useNavigate();
 	const { region, name } = useParams();
+
 	const countryData = getCountryByRegionAndName(region, name);
 	console.log(countryData);
-	return <h2></h2>;
+	return (
+		<Fragment>
+			<Button
+				textContent="Back"
+				icon={<ArrowLeftIcon />}
+				clickHandler={() => navigate('/')}
+			/>
+		</Fragment>
+	);
 }
 
 export default Detail;
