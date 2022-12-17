42d44cf92b4e1b86d7afeee693b63b1773c35323
Q4D4 <dato.kadaria.1@btu.edu.ge>
2022-12-17 19:24:35 +0400

    Added dummy data to Detail page

--- src/pages/Detail/index.jsx
+++ src/pages/Detail/index.jsx
@@ -1,6 +1,6 @@
 import { Fragment } from 'react';
 import { useNavigate, useParams } from 'react-router-dom';
-import { Button } from '../../components';
+import { Button, CountryInfo } from '../../components';
 import { ArrowLeftIcon } from '../../icons';
 import './Detail.css';
 
@@ -21,8 +21,18 @@ function Detail({ getCountryByRegionAndName }) {
 				<img className="Detail__flag" src={countryData.flags.svg} alt="" />
 				<h1 className="Detail__name">{countryData.name.common}</h1>
 				<div className="Detail__info">
-					<div className="Detail__info-basic"></div>
-					<div className="Detail__info-additional"></div>
+					<div className="Detail__info-basic">
+						<CountryInfo prefix="Native name" value={'test'} />
+						<CountryInfo prefix="Population" value={'test'} />
+						<CountryInfo prefix="Region" value={'test'} />
+						<CountryInfo prefix="Sub region" value={'test'} />
+						<CountryInfo prefix="Capital" value={'test'} />
+					</div>
+					<div className="Detail__info-additional">
+						<CountryInfo prefix="Top level domain" value={'test'} />
+						<CountryInfo prefix="Currencies" value={'test'} />
+						<CountryInfo prefix="Languages" value={'test'} />
+					</div>
 				</div>
 			</div>
 		</Fragment>
